d4d919fb68d017056fa92f4163f69b8f7dc18bad
Ananta Podder <31305962+AnantaPodder@users.noreply.github.com>
2022-02-11 18:17:09 +0530

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,5 @@
 import {BotProcessor} from "./bot-processor/bot-processor";
 
 var botProcessor = new BotProcessor();
-const port = process.env.PORT || 3000;
-botProcessor.listen(port, () => {
-  console.log(`Server is ready to listen on port ${port}`);
-});
+
 botProcessor.start();
